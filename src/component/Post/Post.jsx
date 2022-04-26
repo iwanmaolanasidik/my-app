b2586019be4d3f3e4a1823aff2bb8645068f2022
iwanmaolanasidik <iwanmaolanasidik7@gmailcom>
2022-04-26 23:28:00 +0700

    Membuat form input

--- src/component/Post/Post.jsx
+++ src/component/Post/Post.jsx
@@ -9,14 +9,14 @@ const Post = (props) => {
         <img className="card-img-top" src={cocacola} alt="Card image cap" />
         <div className="card-body">
           <p className="card-title">
-            <b>{props.data.title}</b>
+            <b>{props.data.name}</b>
           </p>
-          <p className="card-text">{props.data.body}</p>
+          <p className="card-text">{props.data.address}</p>
         </div>
         <div className="card-footer">
           <button
             className="btn btn-sm btn-danger"
-            onClick={() => props.remove(props.data.id)}
+            onClick={() => props.remove(props.data.student_id)}
           >
             Delete
           </button>
--- src/container/BlogPost/BlogPost.jsx
+++ src/container/BlogPost/BlogPost.jsx
@@ -5,10 +5,15 @@ import axios from "axios";
 class BlogPost extends Component {
   state = {
     post: [],
+    formBlogPost: {
+      name: "",
+      address: "",
+    },
   };
 
   getPostApi = () => {
-    axios.get("http://localhost:3004/posts").then((res) => {
+    axios.get("http://127.0.0.1:8000/api/student").then((res) => {
+      // console.log(res.data);
       this.setState({
         post: res.data,
       });
@@ -16,9 +21,23 @@ class BlogPost extends Component {
   };
 
   handleRemove = (data) => {
-    axios.delete(`http://localhost:3004/posts/${data}`).then((res) => {
+    axios.delete(`http://127.0.0.1:8000/api/student/${data}`).then((res) => {
+      // console.log(res);
       this.getPostApi();
     });
+  };
+
+  handleFormChange = (event) => {
+    let formBlogPostNew = { ...this.state.formBlogPost };
+    formBlogPostNew[event.target.name] = event.target.value;
+    this.setState(
+      {
+        formBlogPost: formBlogPostNew,
+      },
+      () => {
+        console.log(this.state.formBlogPost);
+      }
+    );
   };
 
   componentDidMount() {
@@ -40,9 +59,37 @@ class BlogPost extends Component {
         <div className="row">
           <p>BlogPost Component</p>
           <hr />
+          <div className="form-group">
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              className="form-control"
+              name="name"
+              onChange={this.handleFormChange}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="address">Address</label>
+            <textarea
+              name="address"
+              id="address"
+              cols="30"
+              rows="5"
+              className="form-control"
+              onChange={this.handleFormChange}
+            ></textarea>
+          </div>
+          <div className="form-group mt-2 mb-5 text-end">
+            <div className="btn btn-primary">Simpan</div>
+          </div>
+
           {this.state.post.map((post) => {
             return (
-              <Post key={post.id} data={post} remove={this.handleRemove} />
+              <Post
+                key={post.student_id}
+                data={post}
+                remove={this.handleRemove}
+              />
             );
           })}
         </div>
